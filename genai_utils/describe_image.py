5d4176296848c80257e5d89d45a771338aaa9f85
e346104 <sadananda.narayanappa@lmco.com>
2025-05-30 11:36:26 -0600

    Changes added

--- genai_utils/describe_image.py
+++ genai_utils/describe_image.py
@@ -7,7 +7,7 @@ python -m genai_utils.index_imges --directory </path/to/images>
 from ollama import generate
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DEFAULT_MODEL = 'qwen2.5vl'
+DEFAULT_MODEL = 'qwen2.5'
 
 DEFAULT_SYSTEM_PROMPT = """
 You are an image analyst who has been tasked with describing images.
@@ -24,4 +24,24 @@ Do not explain how you are describing the image. Do not use any "I" statements.
 
 def describe_image(image_data, prompt=DEFAULT_USER_PROMPT, system=DEFAULT_SYSTEM_PROMPT, model=DEFAULT_MODEL):
     result = generate(model=model, prompt=prompt, images=[image_data])
+    return result['response']
+
+
+
+def checkMatched(query, descritpion):
+    prompt=f"""
+You are a great critic who can rank query and descritpion on the scale of 1 to 5.
+You will give 0 if the query is not answered by the descritpion.
+You will give a rank of 5 if query perfectly matches the descritpion.
+Give a rank of intemediate number from 0 to 5 if there is a partial match of key concepts in the descritpion to query.
+A perfect match is when the query matches the description very closely.
+if query does not entail the description give a score of 3
+
+Please rank the following query and descritpion and just return the rank and explain why you gave the ranking
+
+query: {query}\n 
+
+descritpion: {descritpion}.
+"""
+    result=generate(model=DEFAULT_MODEL, prompt=prompt)
     return result['response']
